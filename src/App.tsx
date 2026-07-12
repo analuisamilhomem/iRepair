e3fc6f2c5121d249768c98a364036a575ed50c95
Ana Luisa Milhomem Martins <analuisa.milhomem@gmail.com>
2026-07-12 20:29:34 -0300

    feat(App): organiza cards em layout flex e remove dado de teste

--- src/App.tsx
+++ src/App.tsx
@@ -10,16 +10,7 @@ import {NewServiceForm} from './components/NewServiceForm'
 
 
 export function App() {
-  const [orders, setOrder] = useState<ServiceOrder[]>([
-  {
-    nomeUsuario: "Ana Luísa",
-    modeloAparelho: "iPhone 11",
-    defeito: "Problema na tela",
-    status: "Aberto",
-    dataPedido: "12/07/2026",
-    prazoEntrega: "13/07/2026"
-  }
-  ])
+  const [orders, setOrder] = useState<ServiceOrder[]>([])
 
   function addOrder(newOrder: ServiceOrder) {
    const hoje = new Date().toLocaleDateString('pt-BR')
@@ -34,7 +25,8 @@ export function App() {
     <h2 className= "font-bold px-4 py-4 mt-6 text-2xl">Adicionar uma nova ordem de serviço </h2>
     <NewServiceForm onSave={addOrder} />
     <h2 className= "font-bold px-4 py-4 mt-6 text-2xl">Acompanhar ordens de serviço</h2>
-    {orders.map((order, index) => (
+    <div  className="flex flex-wrap gap-4 mt-4">
+      {orders.map((order, index) => (
       <ServiceCard 
         key={index}
         nomeUsuario={order.nomeUsuario}
@@ -45,6 +37,7 @@ export function App() {
         prazoEntrega={order.prazoEntrega}
       />
     ))}
+    </div>
   </div>
-)
+  )
 }
--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -9,11 +9,11 @@ export interface ServiceOrder {
 
 export function ServiceCard ({nomeUsuario, modeloAparelho, defeito, status, dataPedido, prazoEntrega}: ServiceOrder) {
     return (
-        <div className="border mt-2 ml-4 px-6 py-4 w-80 rounded-lg shadow-md">
+        <div className={`border mt-2 ml-4 px-6 py-4 w-80 rounded-lg shadow-md ${status === 'aberto' ? 'bg-green-200' : 'none'}`}>
             <h3 className= "font-bold"> Cliente: {nomeUsuario} </h3>
             <p>Aparelho: {modeloAparelho}</p>
             <p>Dfeito: {defeito}</p>
-            <p>Status: {status}</p>    
+            <p>Status: {status}</p>
             <p>Data do Pedido: {dataPedido}</p>    
             <p>Prazo de entrega: {prazoEntrega}</p>   
         </div>
